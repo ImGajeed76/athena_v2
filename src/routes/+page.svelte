7465d2fa3048713134715301cae489a071751b43
ImGajeed <76.imgajeed@gmail.com>
2023-10-08 10:48:21 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Let's get cracking bones!</h1>
+		<div>
+			<but
+		</div>
+	</div>
+</div>
