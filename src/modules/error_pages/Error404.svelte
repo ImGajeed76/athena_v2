5020c4ffb3255bd02a58ed23c08ddf7ba53f648e
imgajeed <github.staging362@passmail.net>
2023-11-26 12:54:36 +0100

    Just Saving

--- src/modules/error_pages/Error404.svelte
+++ src/modules/error_pages/Error404.svelte
@@ -1,111 +1,111 @@
 <script lang="ts">
     import {onMount} from "svelte";
-    import {goto} from "$app/navigation";
 
-    let imageElement: HTMLImageElement;
-    let imageSize: { width: number, height: number } = {
-        width: 3582,
-        height: 2048
-    };
 
-    let imageWidth: number;
-    let imageHeight: number;
-    let imageLeft: number;
-    let imageTop: number;
+    let mazeGame;
 
-    let inBookBounds: boolean = false;
+    async function afterMount() {
+        const Phaser = await import('phaser');
 
-    function recalculateObjectCoverSizeAndPosition() {
-        const containerWidth = window.innerWidth;
-        const containerHeight = window.innerHeight;
+        class MazeGame {
+            game: Phaser.Game;
+            maze: number[][];
+            mazeWidth: number;
+            mazeHeight: number;
+            cellSize: number;
 
-        const containerAspectRatio = containerWidth / containerHeight;
-        const imageAspectRatio = imageSize.width / imageSize.height;
+            constructor(mazeWidth, mazeHeight, cellSize) {
+                this.mazeWidth = mazeWidth;
+                this.mazeHeight = mazeHeight;
+                this.cellSize = cellSize;
+                this.maze = this.generateMaze(mazeWidth, mazeHeight);
 
-        let result = {
-            width: 0,
-            height: 0,
-            x: 0,
-            y: 0
-        };
+                this.game = new Phaser.Game({
+                    type: Phaser.AUTO,
+                    width: mazeWidth * cellSize,
+                    height: mazeHeight * cellSize,
+                    parent: 'game_window',
+                    backgroundColor: '#fdfdfd',
+                    scene: {
+                        preload: this.preload.bind(this),
+                        create: this.create.bind(this),
+                        update: this.update.bind(this)
+                    }
+                });
+            }
 
-        if (containerAspectRatio > imageAspectRatio) {
-            result.width = containerWidth;
-            result.height = containerWidth / imageAspectRatio;
-            result.y = (containerHeight - result.height);
-        } else {
-            result.height = containerHeight;
-            result.width = containerHeight * imageAspectRatio;
-            result.x = (containerWidth - result.width) / 2;
+            generateMaze(width: number, height: number) {
+                let maze = Array.from({length: height}, () => Array.from({length: width}, () => 0));
+                let stack: number[][] = [];
+                let x = 0;
+                let y = 0;
+
+                maze[y][x] = 1;
+                stack.push([x, y]);
+
+                while (stack.length > 0) {
+                    let [cx, cy] = stack[stack.length - 1];
+                    let directions = this.getValidDirections(cx, cy, maze, width, height);
+
+                    if (directions.length > 0) {
+                        let [dx, dy] = directions[Math.floor(Math.random() * directions.length)];
+                        maze[cy + dy][cx + dx] = 1;
+                        stack.push([cx + dx, cy + dy]);
+                    } else {
+                        stack.pop();
+                    }
+                }
+
+                return maze;
+            }
+
+            getValidDirections(x: number, y: number, maze: number[][], width: number, height: number) {
+                let directions = [];
+
+                // Check each direction (N, S, E, W) for a valid move
+                if (y > 1 && maze[y - 2][x] === 0) directions.push([0, -2]); // North
+                if (y < height - 2 && maze[y + 2][x] === 0) directions.push([0, 2]); // South
+                if (x > 1 && maze[y][x - 2] === 0) directions.push([-2, 0]); // West
+                if (x < width - 2 && maze[y][x + 2] === 0) directions.push([2, 0]); // East
+
+                return directions;
+            }
+
+            preload() {
+                // Load assets if needed
+            }
+
+            create() {
+            }
+
+            update() {
+                // Update game logic if needed
+            }
         }
 
-        imageWidth = result.width;
-        imageHeight = result.height;
-        imageLeft = result.x;
-        imageTop = result.y;
+        mazeGame = new MazeGame(800 / 10, 800 / 10, 10);
     }
 
-
-    function screenPointToImagePoint(x: number, y: number): { x: number, y: number } {
-        const virtualImageWidth = imageSize.width;
-        const virtualImageHeight = imageSize.height;
-
-        const relativeCoords = {
-            x: x - imageLeft,
-            y: y - imageTop
-        }
-
-        return {
-            x: relativeCoords.x / imageWidth * virtualImageWidth,
-            y: relativeCoords.y / imageHeight * virtualImageHeight
-        };
-    }
 
     onMount(() => {
-        setTimeout(() => {
-            recalculateObjectCoverSizeAndPosition();
-        }, 100);
-
-        window.addEventListener("resize", () => {
-            recalculateObjectCoverSizeAndPosition();
-        });
-
-        imageElement.addEventListener("mousemove", (event) => {
-            const x = event.offsetX;
-            const y = event.offsetY;
-
-            const point = screenPointToImagePoint(x, y);
-
-            const bookBounds = [819, 841, 848, 969]
-            inBookBounds = point.x > bookBounds[0] && point.x < bookBounds[2] && point.y > bookBounds[1] && point.y < bookBounds[3]
-
-            if (inBookBounds) {
-                imageElement.style.cursor = "pointer";
-                imageElement.src = "/athena_404_page_book_out.png";
-            } else {
-                imageElement.style.cursor = "default";
-                imageElement.src = "/athena_404_page.png";
-            }
-        });
-
-        imageElement.addEventListener("click", () => {
-            if (inBookBounds) {
-                imageElement.removeEventListener("mousemove", () => {
-                });
-                imageElement.removeEventListener("click", () => {
-                });
-                window.removeEventListener("resize", () => {
-                });
-                goto("/ee/L7zb6X");
-            }
-        });
+        afterMount();
     });
 </script>
 
-
-<img src="/athena_404_page_book_out.png" alt="Cached Background" class="sr-only">
-
-<div class="w-screen h-screen fixed top-0 left-0">
-    <img bind:this={imageElement} src="/athena_404_page.png" alt="Background"
-         class="w-full h-full object-cover object-bottom">
+<div class="absolute w-screen h-screen top-0 left-0 p-5 grid grid-cols-2 items-center">
+    <div class="w-full">
+        <div class="m-auto w-fit">
+            <h1 class="text-6xl">404</h1>
+            <h2 class="text-2xl">You reached a unknown territory!</h2>
+            <hr class="my-5 h-[2px] bg-black">
+            <p class="text-lg">The page you are looking for does not exist. <br>
+                You can now either just press the buttons below to get back or <br>
+                you could solve a fun little maze on the right to find your exit.</p>
+            <div class="flex-row flex mt-5">
+                <button class="btn variant-filled-secondary btn-3d-secondary mr-3">Home</button>
+                <button class="btn variant-filled-primary btn-3d-primary">Back</button>
+            </div>
+        </div>
+    </div>
+    <div id="game_window"></div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,7 +33,7 @@
         "/signup",
         "/auth/forgot-password",
         "/auth/reset-password",
-        "cards/train/*",
+        "/cards/train(/.*)?",
         "/ee",
         "/"
     ]
@@ -55,7 +55,8 @@
             if (!$loggedIn) {
                 setTimeout(() => {
                     // check if the user is logged in and if not, redirect to login page. If a url ends with /*, then child routes are allowed
-                    if (!$loggedIn && !allowedIfNoUser.some((allowed) => value.route.id?.startsWith(allowed))) {
+                    const regexPattern = new RegExp('^(' + allowedIfNoUser.join('|') + ')$', 'i');
+                    if (!$loggedIn && !regexPattern.test(value.route.id || "/")) {
                         goto('/login?redirect=' + encodeURIComponent(value.route.id || "/"))
                     }
                 }, 1000)
@@ -138,7 +139,8 @@
 </Drawer>
 
 
-<Modal buttonPositive="outline-0 variant-ghost-primary text-black focus:variant-filled-primary hover:variant-filled-primary" buttonNeutral="duration-200 variant-ghost-surface hover:variant-ghost-error"/>
+<Modal buttonPositive="outline-0 variant-ghost-primary text-black focus:variant-filled-primary hover:variant-filled-primary"
+       buttonNeutral="duration-200 variant-ghost-surface hover:variant-ghost-error"/>
 <Toast/>
 
 <div class="h-screen w-screen">
@@ -147,26 +149,26 @@
             <div id="bar" class="sticky grid w-full items-center h-16 px-5 overflow-hidden shadow-xl">
                 <div class="w-full max-w-6xl mx-auto flex justify-between items-center">
                     <div class="flex flex-row items-center">
-                        {#if $page.status === 404}
-                            <a href="/" class="h3 p-2 text-white">Home</a>
-                        {:else}
-                            <div class="flex items-center">
-                                <img src="/athenas_helmet_black.svg" alt="Helmet" class="h-8">
-                                <a href="/" class="h3 p-2">Athena</a>
-                            </div>
-                        {/if}
+                        <div class="flex items-center">
+                            <img src="/athenas_helmet_black.svg" alt="Helmet" class="h-8">
+                            <a href="/" class="h3 p-2">Athena</a>
+                        </div>
                         <div class="mt-2 ml-5">
                             <a href="/cards" class="hover:bg-gray-100 p-2 px-4 rounded-md duration-200">Cards</a>
                         </div>
                     </div>
                     <div class="flex flex-row">
-                        <a href="https://ko-fi.com/athena_learning" target="_blank" class="items-center flex flex-row p-2 rounded-md h-10 justify-center hover:bg-gray-100 pr-2 duration-200">
-                            <img class="h-8" alt="ko-fi" src="https://storage.ko-fi.com/cdn/brandasset/kofi_s_logo_nolabel.png">
+                        <a href="https://ko-fi.com/athena_learning" target="_blank"
+                           class="items-center flex flex-row p-2 rounded-md h-10 justify-center hover:bg-gray-100 pr-2 duration-200">
+                            <img class="h-8" alt="ko-fi"
+                                 src="https://storage.ko-fi.com/cdn/brandasset/kofi_s_logo_nolabel.png">
                         </a>
-                        <a href="https://discord.gg/vNb4suv6Xt" target="_blank" class="items-center flex flex-row p-2 rounded-md h-10 justify-center hover:bg-gray-100 mr-2 duration-200">
+                        <a href="https://discord.gg/vNb4suv6Xt" target="_blank"
+                           class="items-center flex flex-row p-2 rounded-md h-10 justify-center hover:bg-gray-100 mr-2 duration-200">
                             <img alt="discord" class="w-5 h-5 " src="/discord-mark-black.svg"/>
                         </a>
-                        <a href="https://github.com/ImGajeed76/athena_v2" target="_blank" class="items-center flex flex-row p-2 rounded-md h-10 justify-center hover:bg-gray-100 mr-2 duration-200">
+                        <a href="https://github.com/ImGajeed76/athena_v2" target="_blank"
+                           class="items-center flex flex-row p-2 rounded-md h-10 justify-center hover:bg-gray-100 mr-2 duration-200">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16" class="">
                                 <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
                             </svg>
